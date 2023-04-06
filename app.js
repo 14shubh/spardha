22853c01cd0d43024c75a71b57a8fe4ccffd62b6
14shubh <burghateshubham115@gmail.com>
2023-04-07 01:45:13 +0530

    mongodb connection string update

--- app.js
+++ app.js
@@ -12,19 +12,19 @@ const playerRouter = require("./router/player.router");
 const teamRouter = require('./router/team.router');
 const organiserRouter = require('./router/organiser.router');
 const tournamentRouter = require('./router/tournament.router');
-const paymentRouter=require('./router/payment.router');
-const contactRouter=require('./router/contact.router');
+const paymentRouter = require('./router/payment.router');
+const contactRouter = require('./router/contact.router');
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://root:root@cluster0.gffjq.mongodb.net/Spardha?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://spardha:spardha@cluster0.gffjq.mongodb.net/Spardha?retryWrites=true&w=majority")
     .then(() => {
         app.use('/admin', adminRouter);
         app.use('/team', teamRouter);
-        app.use('/payment',paymentRouter);
+        app.use('/payment', paymentRouter);
         app.use('/organiser', organiserRouter);
         app.use('/tournament', tournamentRouter);
-        app.use('/contact',contactRouter);
+        app.use('/contact', contactRouter);
         app.use(playerRouter);
-        app.listen(process.env.PORT || 3000, () => {
+        app.listen(3000, () => {
             console.log("Server Is Running")
             console.log("successfully connected to database....")
         })
